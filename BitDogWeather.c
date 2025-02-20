aff39645a1a69a26b94fa0dbb413bafa0e785ebf
Leo-Luz-code <leonardoluz768@gmail.com>
2025-02-20 13:46:56 -0300

    build: add ssd1306 dependencies and other libs

--- BitDogWeather.c
+++ BitDogWeather.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-
+#include "hardware/i2c.h"
+#include "src/ssd1306.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/timer.h"
+#include "hardware/adc.h"
+#include "hardware/pwm.h"
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
